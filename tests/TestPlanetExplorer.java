ea71bbdc232dcf77cf61fd8cab509b3c2488ce51
somename <someemail>
2016-10-19 14:38:05 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -10,7 +10,7 @@ public class TestPlanetExplorer {
 		PlanetExplorer pe = new PlanetExplorer(100,100, "(5,5), (6,7)");
 		String command = "lrfb";
 		String value = pe.executeCommand(command);
-		assertEquals("Expected explorer to be at 0.0 facing NORTH", "(0,0,NORTH)");
+		assertEquals("Expected explorer to be at 0.0 facing NORTH", "(0,0,NORTH)", value);
 		
 	}
 }
