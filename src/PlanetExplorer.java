7aa581963c593ceb7f707c3db7eefa0e7e1228d2
somename <someemail>
2016-10-19 14:37:27 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,8 @@
 // Finish time:
 
 public class PlanetExplorer {
+	String facing = "NORTH";
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
