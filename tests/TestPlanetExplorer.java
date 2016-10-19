bb63ddf8f360c2bca1b15254bdcc824777bdeadc
somename <someemail>
2016-10-19 14:37:27 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,7 +5,12 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_executeCommand() {
-		fail("Not yet implemented");
+	public void test_executeCommand() throws PlanetExplorerException{
+		
+		PlanetExplorer pe = new PlanetExplorer(100,100, "(5,5), (6,7)");
+		String command = "lrfb";
+		String value = pe.executeCommand(command);
+		assertEquals("Expected explorer to be at 0.0 facing NORTH", "(0,0,NORTH)");
+		
 	}
 }
